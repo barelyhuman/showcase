737e7682eac8ee2ff3bf9538ed4c6e787616237e
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-04-21 14:08:50 +0530

    prettier on push pull

--- services/API.js
+++ services/API.js
@@ -2,19 +2,15 @@ import axios from 'axios';
 const controller = {};
 
 const API = axios.create({
-  baseURL:'https://hacker-news.firebaseio.com/v0'
+  baseURL: 'https://hacker-news.firebaseio.com/v0',
 });
 
-
 controller.getShowStories = () => {
-  return API.get("/showstories.json?print=pretty")
-  .then(data=>data.data);
-}
+  return API.get('/showstories.json?print=pretty').then((data) => data.data);
+};
 
 controller.getItemDetails = (itemId) => {
-  return API.get(`/item/${itemId}.json?print=pretty`)
-  .then(data=>data.data);
-}
-
+  return API.get(`/item/${itemId}.json?print=pretty`).then((data) => data.data);
+};
 
 export default controller;
